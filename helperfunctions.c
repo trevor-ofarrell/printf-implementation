cdd8888d1230f9f89b68af8ef526ec62cf2bf910
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-29 21:50:06 +0000

    added print_num function

--- helperfunctions.c
+++ helperfunctions.c
@@ -4,3 +4,22 @@ int _writeChar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * print_num - prints an integer.
+ * @n:  integer
+ * Description: using recurision of a function we can print the number.
+ * Return: Always 0.
+ */
+void print_num(int n)
+{
+	unsigned int m = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		m = -m;
+	}
+	if (m >= 10)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
+}
