573fcf2b1912037e66469d6712681f7ff9a1c4d0
akeemseymens <akeem.seymens@holbertonschool.com>
2019-07-30 20:11:25 +0000

    linking

--- _printf.c
+++ _printf.c
@@ -4,11 +4,12 @@
  * @format: const char
  * Return: arguments
  */
+int printNsearch(const char *format, op_t op[], va_list list);
+
 int _printf(const char *format, ...)
 {
-	int i = 0, j = 0, r, c = 0;
+	int char_list;
 	va_list list;
-
 	op_t op[] = {
 		{"c", p_char},
 		{"d", p_int},
@@ -18,50 +19,12 @@ int _printf(const char *format, ...)
 		{"s", p_str},
 		{NULL, NULL}
 	};
+
 	if (format == NULL)
 		return (-1);
 
 	va_start(list, format);
-	i = 0;
-	while (format && format[i])
-	{
-		if (format[i] == '%' && format[i + 1] == '%')
-		{
-			_putchar('%');
-			i += 2;
-		}
-		else if (format[i] == '%')
-		{
-			for (j = 0; op[j].ch; j++)
-			{
-				if (format[i + 1] == op[j].ch[0])
-				{
-					r = op[j].ptr(list);
-					if (r == -1)
-					return (-1);
-					c = c + r;
-					break;
-				}
-			}
-			if (!op[j].ch && format[i + 1] != ' ')
-			{
-				if (format[i + 1] != '\0')
-				{
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-				}
-				else
-					return (-1);
-			}
-			i+=2;
-		}
-		else
-		{
-			_putchar(format[i]);
-			i++;
-		}
-		c++;
-	}
+	char_list = printNsearch(format, op, list);
 	va_end(list);
-	return (c);
+	return (char_list);
 }
